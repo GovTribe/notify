12bdfe365db1227c86278dd49001dc003a8591e5
Jay Hariani <jay@govtribe.com>
2014-11-19 15:02:25 -0500

    First commit

---
+++ src/GovTribe/Notify/NotifyServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace GovTribe\Notify;
+
+use Illuminate\Support\ServiceProvider;
+
+class NotifyServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
